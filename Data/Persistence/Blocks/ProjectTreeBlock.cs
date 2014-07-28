927f6498c2945612820d3227d533b51fc4a00f16
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-07-27 22:42:31 -0300

    Adding a ProjectTreeBlock file block class to the persistence library

---
+++ Data/Persistence/Blocks/ProjectTreeBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pixelaria.Data.Persistence.Blocks
+{
+    /// <summary>
+    /// Contains information about the project tree to be displayed on the main interface
+    /// </summary>
+    public class ProjectTreeBlock : Block
+    {
+
+    }
+}
