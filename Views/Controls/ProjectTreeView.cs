6d9ccf17dce04ed5c2a127802817d54a99b9ee77
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-07-28 03:01:43 -0300

    Starting to create a ProjectTreeView to visualize project tree objects

---
+++ Views/Controls/ProjectTreeView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Pixelaria.Data;
+
+namespace Pixelaria.Views.Controls
+{
+    /// <summary>
+    /// Represents a TreeView that can be used to display a ProjectTree object
+    /// </summary>
+    public class ProjectTreeView : RearrangeableTreeView
+    {
+        /// <summary>
+        /// The project tree this project tree view is currently representing
+        /// </summary>
+        ProjectTree loadedProjectTree;
+
+        /// <summary>
+        /// Gets or sets the project tree this project tree view is currently representing
+        /// </summary>
+        public ProjectTree LoadedProjectTree
+        {
+            get { return loadedProjectTree; }
+            set { loadedProjectTree = value; }
+        }
+
+        /// <summary>
+        /// Loads the given ProjectTree object into this ProjectTreeView
+        /// </summary>
+        /// <param name="tree">The project tree to load on this ProjectTreeView</param>
+        private void LoadProjectTree(ProjectTree tree)
+        {
+            this.loadedProjectTree = tree;
+        }
+
+        // 
+        // OnItemDrag event handler. Starts dragging a node
+        // 
+        protected override void OnItemDrag(ItemDragEventArgs e)
+        {
+            TreeNode node = e.Item as TreeNode;
+
+            base.OnItemDrag(e);
+        }
+    }
+}
