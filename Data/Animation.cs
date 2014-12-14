b1b715e7321866fdfc30cb3b3a46c0f57fdb7f1e
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-12-14 20:10:48 -0200

    Implementing .Equals() for the Animation and Frame classes

--- Data/Animation.cs
+++ Data/Animation.cs
@@ -486,6 +486,43 @@ namespace Pixelaria.Data
 
             return null;
         }
+
+        // Override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Animation other = (Animation) obj;
+
+            if (Name != other.Name || _frames.Count != other._frames.Count || Width != other.Width || Height != other.Height ||
+                !ExportSettings.Equals(other.ExportSettings) || !PlaybackSettings.Equals(other.PlaybackSettings))
+                return false;
+
+            // Check frame-by-frame for an innequality
+            for (int i = 0; i < _frames.Count; i++)
+            {
+                if (!_frames[i].Equals(other._frames[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        // Override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Width ^ Height ^ _frames.Count;
+        }
     }
 
     /// <summary>
--- Data/Frame.cs
+++ Data/Frame.cs
@@ -379,5 +379,35 @@ namespace Pixelaria.Data
         {
             _hash = newHash;
         }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            // TODO: write your implementation of Equals() here
+            //throw new NotImplementedException();
+
+            Frame other = (Frame) obj;
+
+            return Utilities.ByteArrayCompare(_hash, other._hash) && _width == other._width &&
+                   _height == other._height && _frameTexture != null && other._frameTexture != null;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return _width ^ _height ^ _id;
+        }
     }
 }
