451d1467e6d6683490e49b4b505b0c1dd63e3ea8
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-12-15 21:36:00 -0200

    Adding one test case for the TextureAtlasTests

--- PixelariaTests/Tests/Data/Exports/TextureAtlasTests.cs
+++ PixelariaTests/Tests/Data/Exports/TextureAtlasTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pixelaria.Data;
 using Pixelaria.Data.Exports;
 using PixelariaTests.Generators;
 
@@ -16,9 +17,21 @@ namespace PixelariaTests.Tests.Data.Exports
     public class TextureAtlasTests
     {
         [TestMethod]
-        public void TestTextureAtlasCreation()
+        public void TestTextureAtlasFrameInsert()
         {
             TextureAtlas atlas = new TextureAtlas(AnimationSheetGenerator.GenerateDefaultAnimationExportSettings(), "TestAtlas");
+
+            Animation anim = AnimationGenerator.GenerateAnimation("TestAnim1", 16, 16, 10);
+
+            foreach (var frame in anim.Frames)
+            {
+                atlas.InsertFrame(frame);
+            }
+            
+            Assert.AreEqual(anim, atlas.GetAnimationsOnAtlas()[0], "After adding frames of an animation to an atlas, the animation should be listed on the atlas' GetAnimationsOnAtlas()");
+            Assert.AreEqual(anim.FrameCount, atlas.FrameCount, "When adding frames to an atlas, all valid frames passed to the InsertFrame() should be counted in it");
+            Assert.IsTrue(!anim.Frames.Where((t, i) => !ReferenceEquals(t, atlas.FrameList[i])).Any(),
+                "When adding frames to an atlas, all valid frames passed to the InsertFrame() should be counted in it");
         }
     }
 }
