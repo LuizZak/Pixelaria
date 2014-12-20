23108e83244a0da9be30986fff4ddd22300491f1
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2014-12-20 02:44:00 -0200

    Adding missing license headers

--- Pixelaria/Data/Factories/FrameFactory.cs
+++ Pixelaria/Data/Factories/FrameFactory.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/*
+    Pixelaria
+    Copyright (C) 2013 Luiz Fernando Silva
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    The full license may be found on the License.txt file attached to the
+    base directory of this project.
+*/
 
 using Pixelaria.Controllers;
-using Pixelaria.Data;
 
 namespace Pixelaria.Data.Factories
 {
@@ -16,7 +32,7 @@ namespace Pixelaria.Data.Factories
         /// <summary>
         /// A reference to the main controller
         /// </summary>
-        private Controller controller;
+        private readonly Controller _controller;
 
         /// <summary>
         /// Initializes a new instance of the DefaultFrameFactory class
@@ -24,7 +40,7 @@ namespace Pixelaria.Data.Factories
         /// <param name="controller">The controller to attach this factory to</param>
         public DefaultFrameFactory(Controller controller)
         {
-            this.controller = controller;
+            _controller = controller;
         }
 
         /// <summary>
@@ -37,9 +53,10 @@ namespace Pixelaria.Data.Factories
         /// <returns>The newly created frame</returns>
         public Frame CreateFrame(int width, int height, Animation anim = null, bool initHash = true)
         {
-            Frame frame = new Frame(anim, width, height, initHash);
-
-            frame.ID = controller.CurrentBundle.GetNextValidFrameID();
+            Frame frame = new Frame(anim, width, height, initHash)
+            {
+                ID = _controller.CurrentBundle.GetNextValidFrameID()
+            };
 
             return frame;
         }
@@ -52,7 +69,7 @@ namespace Pixelaria.Data.Factories
         {
             Frame newFrame = frame.Clone();
 
-            newFrame.ID = controller.CurrentBundle.GetNextValidFrameID();
+            newFrame.ID = _controller.CurrentBundle.GetNextValidFrameID();
 
             return newFrame;
         }
