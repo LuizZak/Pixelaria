a4bd51e7217e0b661a3cebda1276285b4008e677
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-04 17:30:01 -0300

    Implementing ISpatialReference in LayoutGuide

--- PixUI/LayoutSystem/LayoutGuide.cs
+++ PixUI/LayoutSystem/LayoutGuide.cs
@@ -29,7 +29,7 @@ namespace PixUI.LayoutSystem
     /// <summary>
     /// A rectangular area that can interact with the layout constraint system.
     /// </summary>
-    public class LayoutGuide : ILayoutVariablesContainer
+    public class LayoutGuide : ISpatialReference, ILayoutVariablesContainer
     {
         /// <summary>
         /// The view that this layout guide is currently contained within, or <c>null</c>, in case it is not
@@ -41,6 +41,8 @@ namespace PixUI.LayoutSystem
         private readonly LayoutVariables _layoutVariables;
 
         LayoutVariables ILayoutVariablesContainer.LayoutVariables => _layoutVariables;
+
+        internal Matrix2D Transform => Matrix2D.Translation(FrameOnParent.Left, FrameOnParent.Top);
 
         public ISpatialReference ParentSpatialReference => ownerView;
 
@@ -65,6 +67,88 @@ namespace PixUI.LayoutSystem
             ownerView?.SetNeedsLayout();
         }
 
+
+        /// <summary>
+        /// Converts a point from a given <see cref="ISpatialReference"/>'s local coordinates to this
+        /// layout guide's coordinates.
+        /// 
+        /// If <see cref="from"/> is null, converts from screen coordinates.
+        /// </summary>
+        public Vector ConvertFrom(Vector point, ISpatialReference from)
+        {
+            // Convert point to global, if it's currently local to 'from'
+            var global = point;
+            if (from != null)
+                global = from.GetAbsoluteTransform() * point;
+
+            var matrix = GetAbsoluteTransform().Inverted();
+            return global * matrix;
+        }
+
+        /// <summary>
+        /// Converts a point from this <see cref="ISpatialReference"/>'s local coordinates to a given
+        /// layout guide's coordinates.
+        /// 
+        /// If <see cref="to"/> is null, converts from this node to screen coordinates.
+        /// </summary>
+        public Vector ConvertTo(Vector point, ISpatialReference to)
+        {
+            if (to == null)
+                return point * GetAbsoluteTransform();
+
+            return to.ConvertFrom(point, this);
+        }
+
+        /// <summary>
+        /// Converts an AABB from a given <see cref="ISpatialReference"/>'s local coordinates to this
+        /// layout guide's coordinates.
+        /// 
+        /// If <see cref="from"/> is null, converts from screen coordinates.
+        /// </summary>
+        public AABB ConvertFrom(AABB aabb, ISpatialReference from)
+        {
+            // Convert point to global, if it's currently local to from
+            var global = aabb;
+            if (from != null)
+                global = aabb.TransformedBounds(from.GetAbsoluteTransform());
+
+            var matrix = GetAbsoluteTransform().Inverted();
+            return global.TransformedBounds(matrix);
+        }
+
+        /// <summary>
+        /// Converts an AABB from this <see cref="ISpatialReference"/>'s local coordinates to a given
+        /// layout guide's coordinates.
+        /// 
+        /// If <see cref="to"/> is null, converts from this node to screen coordinates.
+        /// </summary>
+        public AABB ConvertTo(AABB aabb, ISpatialReference to)
+        {
+            if (to == null)
+                return aabb.TransformedBounds(GetAbsoluteTransform());
+
+            return to.ConvertFrom(aabb, this);
+        }
+
+        /// <summary>
+        /// Gets the absolute matrix for this layout guide's coordinates system.
+        /// 
+        /// Multiplying a point (0, 0) by this matrix results in a point that lands on
+        /// the top-left corner of this layout guide's coordinate system.
+        /// </summary>
+        public Matrix2D GetAbsoluteTransform()
+        {
+            if (ownerView == null)
+            {
+                return Transform;
+            }
+
+            var total = ownerView.GetAbsoluteTransform();
+            var local = Transform;
+
+            return Matrix2D.Multiply(in local, in total);
+        }
+        
         AABB ILayoutVariablesContainer.BoundsForRedrawOnScreen()
         {
             return FrameOnParent;
